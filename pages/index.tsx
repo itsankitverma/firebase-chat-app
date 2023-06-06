e25f6f280f1f2df076f22d4f0df4909ddddae702
Ankit Verma <imankit.v5@gmail.com>
2023-06-06 23:19:16 +0530

    added index contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,14 @@ import useFirebase from "@/lib/useFirebase";
 import IndexPlaceholder from "@/components/indexPlaceholder";
 
 const Index = () => {
-  const { user } = useFirebase();
+  const { user, handleSignInWithGoogle } = useFirebase();
   return (
-    <div className="flex ">
+    <div
+      className={`flex ${
+        !user &&
+        "bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90% h-screen"
+      }`}
+    >
       {user && (
         <div className="flex w-full">
           <div>
@@ -20,6 +25,25 @@ const Index = () => {
               <IndexPlaceholder />
             </div>
           </div>
+        </div>
+      )}
+      {!user && (
+        <div className="flex flex-col justify-center gap-10 items-center w-full py-28 md:py-40">
+          <div className="flex flex-col justify-center items-center gap-4 w-full px-2 text-center">
+            <h1 className="text-2xl md:text-5xl font-bold">
+              ConnectMe: Engage, Chat, and Discover.
+            </h1>
+            <p className="md:w-[40rem] text-center">
+              Seamless connections. Engaging conversations. Endless
+              possibilities. ConnectMe brings people together
+            </p>
+          </div>
+          <button
+            onClick={() => handleSignInWithGoogle()}
+            className="p-3 bg-[#4656A1] text-white rounded-lg"
+          >
+            Get Started
+          </button>
         </div>
       )}
     </div>
